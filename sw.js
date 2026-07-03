5d15caede025e5dfeb8f5229a695726d5ea500ff
pablobianchino <pablobianchino@gmail.com>
2026-07-03 14:28:50 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,12 @@
-// Un Service Worker básico solo para cumplir el requisito de PWA.
+const CACHE_NAME = 'gastos-priel-v1';
+
+self.addEventListener('install', (event) => {
+    self.skipWaiting();
+});
+
 self.addEventListener('fetch', function(event) {
-  // Pasa de largo, no intercepta las peticiones (siempre requiere internet)
+    // Si es un pedido de navegación (abrir la app), intentar buscar en red
+    event.respondWith(fetch(event.request).catch(function() {
+        return caches.match(event.request);
+    }));
 });
