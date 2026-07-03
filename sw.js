693ecfe6c63825e820a19b361c956590074d92a4
pablobianchino <pablobianchino@gmail.com>
2026-07-03 14:16:58 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,4 @@
+// Un Service Worker básico solo para cumplir el requisito de PWA.
+self.addEventListener('fetch', function(event) {
+  // Pasa de largo, no intercepta las peticiones (siempre requiere internet)
+});
